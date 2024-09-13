6b30a9888a75563bae32aee9bba92b38eb7a314d
Lucifer! <6011234+rondelli@users.noreply.github.com>
2024-09-12 21:54:09 -0300

    Fix one task can have multiple members

--- src/core/TaskAssigner.java
+++ src/core/TaskAssigner.java
@@ -1,12 +1,18 @@
 package core;
 
 import java.util.Map;
+import java.util.Set;
 
 public class TaskAssigner {
 	
-	private Map<Task, Member> tasksAssigned;
+	private Map<Task, Set<Member>> tasksAssigned;
 	
+	public TaskAssigner() {
+		this.tasksAssigned = null;
+	}
+
 	public void assignTask(Task t, Member m) {
+		// tasksAssigned ← task, member
 		System.out.println("Viva Perón!");
 	}
 
