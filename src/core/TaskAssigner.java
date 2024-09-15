f1baa63851eab0434300a0aec4defc772fbf383b
Xime <xebertz@campus.ungs.edu.ar>
2024-09-15 01:17:27 -0300

    Add TaskAssigner impl - extends from Observable

--- src/core/TaskAssigner.java
+++ src/core/TaskAssigner.java
@@ -3,13 +3,14 @@ package core;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
+import java.util.Observable;
 import java.util.Observer;
 import java.util.Set;
 
 // Asigna tareas a miembros, y sabe qué tareas se asignaron a cuáles miembros.
-public class TaskAssigner {
+@SuppressWarnings("deprecation")
+public class TaskAssigner extends Observable {
 	
-	@SuppressWarnings("deprecation")
 	private Set<Observer> observers;
 	private Map<Task, Set<Member>> assignedTasks;	// not taskAssigned jsajaja
 
@@ -23,12 +24,27 @@ public class TaskAssigner {
 	public void assignTask(Task t, Member m) {
 		assignedTasks.putIfAbsent(t, new HashSet<>());
 		assignedTasks.get(t).add(m);
-		System.out.println("Viva Perón!");
+		
+		notifyObservers(t, m);
+		//System.out.println("Viva Perón!");
 	}
 	
 	// TODO: think about if we really need this
 	public boolean hasChanged() {
 		return false;
 	}
+	
+	public void addObserver(Observer observer) {
+        observers.add(observer);
+    }
+
+    public void removeObserver(Observer observer) {
+        observers.remove(observer);
+    }
+    
+    private void notifyObservers(Task t, Member m) {
+    	String notification = "Se le asignó la tarea " + t.getDescription() + " al miembro " + m.getName();
+        observers.forEach(observer -> observer.update(this, notification));
+    }
 
 }
