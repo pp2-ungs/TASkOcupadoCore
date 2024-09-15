1331c6188841e9f04d7c0ddc4ccc26ccf020e7ce
Xime <xebertz@campus.ungs.edu.ar>
2024-09-15 00:34:48 -0300

    Add ConcreteNotificator

---
+++ src/core/ConcreteNotificator.java
@@ -0,0 +1,17 @@
+package core;
+
+import java.util.Observable;
+
+public class ConcreteNotificator extends Notificator {
+
+	@SuppressWarnings("deprecation")
+	@Override
+	public void update(Observable o, Object arg) {
+		System.out.println(arg);
+	}
+	
+	
+	public void notify(Object msg) {
+		// TODO: how's this different from the other one?
+	}
+}
--- src/core/Notificator.java
+++ src/core/Notificator.java
@@ -4,5 +4,5 @@ import java.util.Observer;
 
 @SuppressWarnings("deprecation")
 public abstract class Notificator implements Observer {
-
+	// TODO: check this, it's doing nothing ;)
 }
