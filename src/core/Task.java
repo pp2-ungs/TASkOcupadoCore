ccda9e5d62dd6b101ae97e810ad24baa2822aadd
Xime <xebertz@campus.ungs.edu.ar>
2024-09-18 22:11:28 -0300

    Fix Task - add void constructor and setter

--- src/core/Task.java
+++ src/core/Task.java
@@ -7,12 +7,18 @@ public class Task {
 	
 	private String description;
 	
+	public Task() { }
+	
 	public Task(String description) {
 		this.description = description;
 	}
 
 	public String getDescription() {
 		return description;
+	}
+	
+	public void setDescription(String d) {
+		description = d;
 	}
 
 	@Override
