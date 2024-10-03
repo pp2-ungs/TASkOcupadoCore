4c82d4efd928459414bbbd14f7af3f9eb5643e04
xebertz <139810831+xebertz@users.noreply.github.com>
2024-10-03 10:19:59 -0300

    Comment Notificator.java - se va?

--- src/core/Notificator.java
+++ src/core/Notificator.java
@@ -1,6 +1,8 @@
 package core;
 
 // FIXME
+// X: este probablemente se vaya, pero depende de si usamos un DTO. en ese caso,
+// Lautaro había dicho que se puede solucionar el tema de los dos métodos
 public interface Notificator extends Observer {
 
     // FIXME: notify(TASkOcupadoDTO taskOcupadoDTO)
