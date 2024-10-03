9fc6f61f9a193c1d7b5ab295943306c7381a2d97
xebertz <139810831+xebertz@users.noreply.github.com>
2024-10-03 10:21:28 -0300

    Comment Observer.java - abstraction leak

--- src/core/Observer.java
+++ src/core/Observer.java
@@ -4,6 +4,7 @@ package core;
 public interface Observer {
 
     // FIXME: update(TASkOcupadoDTO dto)
+    // no puede ser un TASkOcupado dto, es abstraction leak
     public void update(NotificationDTO notificationDTO);
 
 }
