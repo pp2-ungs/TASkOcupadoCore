45afee5f23609fb3cc61c43aae80bd996f14b516
Xime <xebertz@campus.ungs.edu.ar>
2024-10-06 13:43:58 -0300

    Fix assignation

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,12 +1,28 @@
 package main;
 
+import core.Member;
 import core.TASkOcupado;
+import core.Task;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
         TASkOcupado t = new TASkOcupado("");
         
-        System.out.println(t.getMembers());
-        System.out.println(t.getTasks());
+        Set<Task> tasks = t.getTasks();
+        Set<Member> members = t.getMembers();
+        
+        System.out.println(tasks);
+        System.out.println(members);
+        
+        List<Task> taskList = new ArrayList<>(tasks);
+        
+        for(Member m : members) {
+            if (m.getName().equals("Ximena Ebertz")) {
+                t.assignTask(taskList.get(0), m);
+            }
+        }
     }
 }
