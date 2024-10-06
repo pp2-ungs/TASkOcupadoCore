e863964b5ba73f40312d9fbc8a716b4617382a33
Lucifer! <6011234+rondelli@users.noreply.github.com>
2024-10-06 13:51:13 -0300

    Refactor Factory → Helper

---
+++ src/core/TASkOcupadoHelper.java
@@ -0,0 +1,50 @@
+package core;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+// Responsabilidad: inicializar el Core.
+public class TASkOcupadoHelper {
+
+    private Properties properties;
+
+    public TASkOcupadoHelper(String path) {
+        this.properties = new Properties();
+
+        path = path == null || path.isEmpty() ? CoreSettings.PROPERTIES_FILE : path;
+
+        try {
+            properties.load(new FileInputStream(path));
+        } catch (IOException e) {
+            System.out.println("?properties I/O error: " + path);
+        }
+    }
+
+    public Set<Observer> getObservers() {
+        Discoverer discoverer = new Discoverer(properties);
+
+        Set<Object> objects = discoverer.discover(Observer.class);
+        Set<Observer> observers = objects.stream()
+            .map(obj -> (Observer) obj)
+            .collect(Collectors.toSet());
+        return observers;
+    }
+
+    private DataSetLoader getLoader() {
+        PluginFactory plugin = new PluginFactory(properties);
+        DataSetLoader loader = (DataSetLoader) plugin.getPlugin(DataSetLoader.class);
+        return loader;
+    }
+
+    public Set<Task> getTasks() {
+        return getLoader().loadSet(Task.class);
+    }
+
+    public Set<Member> getMembers() {
+        return getLoader().loadSet(Member.class);
+    }
+
+}
