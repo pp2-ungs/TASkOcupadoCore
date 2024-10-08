d96c4191796d6ad202de3c842a68a185e83d64a2
xebertz <139810831+xebertz@users.noreply.github.com>
2024-10-08 12:59:40 -0300

    Clean PluginFactory.java

--- src/tools/PluginFactory.java
+++ src/tools/PluginFactory.java
@@ -6,28 +6,6 @@ import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
 import java.util.Properties;
 
-// ???
-// Preguntar a Javier, si está bien así.
-//
-// En el PEAA, está con `properties` como variable estática, y `getPlugin()`
-// como método estático, y hardcodea el path del archivo de configuración.
-//
-// Para mí, está mal que esté el path hardcodeado, por eso lo reescribí como
-// variable de instancia, y con método de instancia, para que se le pueda pasar
-// el path al constructor.
-
-// Responsabilidad: obtener una implementación de una "Separated Interface"
-// en runtime.
-
-// NOTA IMPORTANTE: esto me parece que no tiene que ser de tipo T. 
-// getPlugin tiene sentido que reciba un object, ya que le estamos 
-// diciendo qué object recibir.
-
-// si esto es de tipo T, es rarísimo, porque esto devuelve un DataSetLoader,
-// también de tipo T.
-
-// hay que ver si hay alguna justificación que convenza que esto NO es de tipo
-// T, y agarrarnos a eso o pensar cómo hacer que ande.
 public class PluginFactory {
 
     private Properties properties;
@@ -35,8 +13,7 @@ public class PluginFactory {
     public PluginFactory(Properties properties) {
         this.properties = properties;
     }
-
-    // queda así
+    
     public Object getPlugin(Class classObject) {
         String type = classObject.getName();
         String className = properties.getProperty(type);
