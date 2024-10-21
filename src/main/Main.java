cd1748b8abfbf52b2bc95fc488c811b6632ca5f5
Xime <xebertz@campus.ungs.edu.ar>
2024-10-20 22:40:43 -0300

    Delete assignment

--- src/main/Main.java
+++ src/main/Main.java
@@ -16,7 +16,5 @@ public class Main {
         
         System.out.println(tasks);
         System.out.println(members);
-        
-        taskOcupado.assignTask(new Task("Ir al gym"), new Member("Ximena Ebertz"));
     }
 }
